9cf290ff9a893ef5e3a3d03f7d4aa0e47441bd0e
Tereza Holm <holm.tereza@gmail.com>
2024-10-17 11:12:24 +0200

    Finished step 1.11

--- part1/unicafe/src/components/StatisticLine.jsx
+++ part1/unicafe/src/components/StatisticLine.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 export const StatisticLine = ({ text, value }) => {
   return (
-    <div>
-      {text}: {value}
-    </div>
+    <tr>
+      <td>{text}:</td> <td>{value}</td>
+    </tr>
   );
 };
