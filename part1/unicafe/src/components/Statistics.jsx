5633a7f3686d33cb8c362f9fabb6650108b6997f
Tereza Holm <holm.tereza@gmail.com>
2024-10-17 09:54:40 +0200

    Finished step 1.8 and 1.9

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -3,9 +3,13 @@ export const Statistics = ({ goodFeedback, neutralFeedback, badFeedback }) => {
   const all = goodFeedback + neutralFeedback + badFeedback;
   const average = all > 0 ? (goodFeedback - badFeedback) / all : 0;
   const positive = all > 0 ? goodFeedback / all : 0;
-  return (
+  return all === 0 ? (
     <div>
-      <h1>Statistics:</h1>
+      <h1>Statistics</h1>No feedback given.
+    </div>
+  ) : (
+    <div>
+      <h1>Statistics</h1>
       <div>Good: {goodFeedback}</div>
       <div>Neutral: {neutralFeedback}</div>
       <div>Bad: {badFeedback} </div>
