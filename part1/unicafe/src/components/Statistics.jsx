bddf167fdcd086e905c543ce74ad84e88f94d29f
Tereza Holm <holm.tereza@gmail.com>
2024-10-17 09:46:23 +0200

    Finished step 1.7

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,11 +1,17 @@
 // eslint-disable-next-line react/prop-types
 export const Statistics = ({ goodFeedback, neutralFeedback, badFeedback }) => {
+  const all = goodFeedback + neutralFeedback + badFeedback;
+  const average = all > 0 ? (goodFeedback - badFeedback) / all : 0;
+  const positive = all > 0 ? goodFeedback / all : 0;
   return (
     <div>
       <h1>Statistics:</h1>
       <div>Good: {goodFeedback}</div>
       <div>Neutral: {neutralFeedback}</div>
       <div>Bad: {badFeedback} </div>
+      <div>All: {all}</div>
+      <div>Average: {average}</div>
+      <div>Positive: {positive}%</div>
     </div>
   );
 };
