41e1c37f585d728384545b39f4238478013feabd
Tereza Holm <holm.tereza@gmail.com>
2024-10-22 11:15:51 +0200

    Finished courseinfo part 7

--- part1/courseinfo/src/components/Course.jsx
+++ part1/courseinfo/src/components/Course.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/prop-types */
 import { Content } from "./Content";
 import { Header } from "./Header";
+import { Total } from "./Total";
 
 export const Course = ({ course }) => {
   return (
     <div>
       <Header courseName={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
