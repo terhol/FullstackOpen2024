e04ba5b7b1016a0a3db5f2869a9a1c884a051a64
Tereza Holm <holm.tereza@gmail.com>
2024-10-30 10:06:37 +0100

    Finished phonebook step 1

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,7 +1,5 @@
 /* eslint-disable react/jsx-key */
-
 import { Part } from "./Part";
-
 /* eslint-disable react/prop-types */
 export const Content = ({ parts }) => {
   return (
---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/jsx-key */
+import { useState } from "react";
+
+export const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addNewName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNewNameSubmit = (event) => {
+    event.preventDefault();
+    const newNameObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(newNameObject), newNameObject);
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleNewNameSubmit}>
+        <div>
+          Name: <input onChange={addNewName} value={newName} />
+        </div>
+        <div>
+          <button type="submit">Add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </div>
+  );
+};
